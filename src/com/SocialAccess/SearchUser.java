fd03bdab3ddbf7b1d2e4080ca93e72f8c6958d84
derekmizak <dm@dmzit.com>
2015-03-19 09:49:23 +0000

    Fixing - Twitter search paging

--- src/com/SocialAccess/SearchUser.java
+++ src/com/SocialAccess/SearchUser.java
@@ -6,32 +6,32 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 
-/**
- *
- * @author kundan
- */
 public class SearchUser {
     
     public void searchOnTwitter(){
         int page=1;
+        int numberofusers=0;
         try{
         Twitter twitter = new TwitterFactory().getInstance();
-       ResponseList<User> users= twitter.searchUsers("Pavel Gladyshev", page);
+       ResponseList<User> users;
+               //= twitter.searchUsers("Microsoft", page);
        
        do {
-                //users = twitter.searchUsers(args[0], page);
+                users = twitter.searchUsers("mizak", page);
                 for (User user : users) {
                     if (user.getStatus() != null) {
                         System.out.println("@" + user.getScreenName() + " - " + user.getFollowersCount());
+                       numberofusers++;
                     } else {
                         // the user is protected
                         System.out.println("@" + user.getScreenName());
+                       
                     }
                 }
                 page++;
-                System.out.println(page);
+                //System.out.println(page);
             } while (users.size() != 0 && page < 50);
-            System.out.println("I'v found " + users.size());
+            System.out.println("I'v found " + numberofusers );
        
        
         }catch(Exception ex){
